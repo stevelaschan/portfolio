a07d93bdb1e0b3c1665d969523f1c4d7b7d57da9
Stefan Laschan <st.laschan@gmail.com>
2022-01-10 17:08:47 +0100

    Änderungen 2

--- content/main.js
+++ content/main.js
@@ -5,7 +5,7 @@ export const attributes = {
       link: '/#portofolio',
       text: 'My work',
     },
-    heading: "I'm Paco Cu, and I create",
+    heading: "I'm Stefan Laschan, and I create",
     image: '/img/hero.jpeg',
     secondButton: { icon: 'whatsapp', link: '/#contact', text: 'Contact' },
     social: {
